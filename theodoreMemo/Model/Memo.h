7b731d7091bd001499e090665c478d08f0e0de74
theodore <bethejustice@gmail.com>
2020-02-10 00:33:20 +0900

    feat Memo Model
     - memo Model that has content, date member value

---
+++ theodoreMemo/Model/Memo.h
@@ -0,0 +1,18 @@
+//
+// Created by theodore on 2020/02/10.
+// Copyright (c) 2020 mobile-native. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Memo : NSObject
+
+@property (strong, nonatomic) NSString* content;
+@property (strong, nonatomic) NSDate* insertDate;
+
+- (instancetype)initWithContent:(NSString *)content;
+
++ (NSArray *)dummyMemoList;
+
+@end
---
+++ theodoreMemo/Model/Memo.m
@@ -0,0 +1,28 @@
+//
+// Created by theodore on 2020/02/10.
+// Copyright (c) 2020 mobile-native. All rights reserved.
+//
+
+#import "Memo.h"
+
+
+@implementation Memo {
+
+}
+
+- (instancetype)initWithContent:(NSString *)content {
+    self = [super init];
+    if (self != nil) {
+        self.content = content;
+    }
+    return nil;
+}
+
++ (NSArray *)dummyMemoList {
+    Memo * memo1 = [[Memo alloc] initWithContent:@"dummy data 001"];
+    Memo * memo2 = [[Memo alloc] initWithContent:@"dummy data 002"];
+
+    return @[memo1, memo2];
+}
+
+@end
