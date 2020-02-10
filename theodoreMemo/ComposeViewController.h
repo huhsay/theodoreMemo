458198ce9061f6acde691b28aae047843de7a98d
theodore <bethejustice@gmail.com>
2020-02-11 00:23:32 +0900

    feat close button
     - add ComposeViewController
     - add close action

---
+++ theodoreMemo/ComposeViewController.h
@@ -0,0 +1,12 @@
+//
+// Created by theodore on 2020/02/11.
+// Copyright (c) 2020 mobile-native. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface ComposeViewController : UIViewController
+- (IBAction)close:(id)sender;
+@end
---
+++ theodoreMemo/ComposeViewController.m
@@ -0,0 +1,19 @@
+//
+// Created by theodore on 2020/02/11.
+// Copyright (c) 2020 mobile-native. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+
+
+@implementation ComposeViewController {
+
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (IBAction)close:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
