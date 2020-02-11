a66ed624a1edb5f2b7d12ac10fe0078be5805de8
theodore <bethejustice@gmail.com>
2020-02-12 00:37:45 +0900

    feat memo detail view storyboard
     - memo detail view / view contoller

---
+++ theodoreMemo/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  theodoreMemo
+//
+//  Created by theodore on 2020/02/12.
+//  Copyright © 2020 mobile-native. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ theodoreMemo/DetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.m
+//  theodoreMemo
+//
+//  Created by theodore on 2020/02/12.
+//  Copyright © 2020 mobile-native. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
