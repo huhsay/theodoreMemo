ce2308a47ddfb5e5ffb691353d3a8f67215b7dab
theodore <bethejustice@gmail.com>
2020-02-12 01:49:41 +0900

    feat detailViewController set memo data from memolistTVC

--- theodoreMemo/DetailViewController.m
+++ theodoreMemo/DetailViewController.m
@@ -10,14 +10,20 @@
 #import "Memo.h"
 
 @interface DetailViewController () <UITableViewDataSource>
-
+@property (strong, nonatomic) NSDateFormatter *formatter;
 @end
 
 @implementation DetailViewController
+@synthesize memo;
 
 #pragma mark - life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    self.formatter = [[NSDateFormatter alloc] init];
+    self.formatter.dateStyle = NSDateFormatterLongStyle;
+    self.formatter.timeStyle = NSDateFormatterMediumStyle;
+    self.formatter.locale = [NSLocale localeWithLocaleIdentifier:@"Ko_kr"]; // data format korea
     // Do any additional setup after loading the view.
 }
 
@@ -31,9 +37,13 @@
 
     if (indexPath.row == 0) {
         UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"memoCell" forIndexPath:indexPath];
+
+        cell.textLabel.text = self.memo.content;
+
         return cell;
     } else if (indexPath.row == 1) {
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"dateCee" forIndexPath:indexPath];
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"dateCell" forIndexPath:indexPath];
+        cell.textLabel.text = [self.formatter stringFromDate: self.memo.insertDate];
         return  cell;
     }
 
